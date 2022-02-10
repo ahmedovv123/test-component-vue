a6461ee082988c3ac146a8dc628e854200c5356c
ahmedovv123 <ahmedovv123@gmail.com>
2022-02-10 22:04:05 +0200

    Upload tester vue project

---
+++ tester/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <TestComponent />
+</template>
+
+<script>
+import TestComponent from 'test-component-l3y21'
+export default {
+  name: 'App',
+  components: {
+    TestComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
